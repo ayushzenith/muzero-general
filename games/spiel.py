27f015ef18069ed6b38171eda9089f74b85d0455
Ayush Zenith <ayushp.zenith@gmail.com>
2023-04-24 02:00:27 -0400

    Update spiel.py

--- games/spiel.py
+++ games/spiel.py
@@ -20,7 +20,7 @@ except ImportError:
     )
 
 # The game you want to run. See https://github.com/deepmind/open_spiel/blob/master/docs/games.md for a list of games
-game = pyspiel.load_game("tic_tac_toe")
+game = pyspiel.load_game("chess")
 
 
 class MuZeroConfig:
@@ -29,7 +29,6 @@ class MuZeroConfig:
         # More information is available here: https://github.com/werner-duvaud/muzero-general/wiki/Hyperparameter-Optimization
 
         self.game = game
-
         self.seed = 0  # Seed for numpy, torch and the game
         self.max_num_gpus = None  # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
 
@@ -44,8 +43,6 @@ class MuZeroConfig:
         # Evaluate
         self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
         self.opponent = "self"  # Hard coded agent that MuZero faces to assess his progress in multiplayer games. It doesn't influence training. None, "random" or "expert" if implemented in the Game class
-
-
 
         ### Self-Play
         self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
